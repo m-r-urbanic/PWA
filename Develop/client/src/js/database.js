0d64245a8f12eb98b8fafad9d2a3283496405e01
Madeline Urbanic <madelinerurbanic@gmail.com>
2024-01-17 21:10:47 -0500

    post to database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+export const putDb = async (content) => {
+  //error if not implemented
+  console.error('putDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ jate: content });
+  const result = await request;
+  //on success, log the data that was saved
+  console.log('Data saved to the openDB', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (id) => {
+  console.error('getDb not implemented');
+
+}
+
 
 initdb();
