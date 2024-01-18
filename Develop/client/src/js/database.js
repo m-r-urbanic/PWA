f3ebbd803a4ebff88d1cce122700a30c9b9e380e
Madeline Urbanic <madelinerurbanic@gmail.com>
2024-01-17 21:25:29 -0500

    add manifest

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,27 +12,27 @@ const initdb = async () =>
     },
   });
 
-export const putDb = async (content) => {
-  //error if not implemented
-  console.error('putDb not implemented');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.add({ jate: content });
-  const result = await request;
-  //on success, log the data that was saved
-  console.log('Data saved to the openDB', result);
+  export const putDb = async (content) => {
+    //error if not implemented
+    console.error('putDb not implemented');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ jate: content });
+    const result = await request;
+    //on success, log the data that was saved
+    console.log('Data saved to the openDB', result);
 }
 
 export const getDb = async (id) => {
-  console.error('getDb not implemented');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(id);
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+    console.error('getDb not implemented');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(id);
+    const result = await request;
+    console.log('result.value', result);
+    return result;
 }
 
 initdb();
